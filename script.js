9fcf09cd3df90c0156484c34912debc1aa601073
Amanda Farias <amannndafarias@gmail.com>
2024-05-20 11:59:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
-function carregar() {
-    let msg = window.document.getElementById('msg')
-    let img = window.document.getElementById('imagem')
+function verificar() {
     let data = new Date()
-    let hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
-    // bom dia!
-    img.src = 'fotomorning.gif'
-    document.body.style.background = '#f8da97'
-    } else if (hora >= 12 && hora < 18) {
-    // boa tarde!
-    img.src = 'fotoafternoon.gif'
-    document.body.style.background = '#e7b78c'
+    let ano = data.getFullYear()
+    let fano = document.getElementById('txtano')
+    let res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-    // boa noite!
-    img.src = 'fotonight.gif'
-    document.body.style.background = '#042244'
+        let fsex = document.getElementsByName('radsex')
+        let idade = ano - Number(fano.value)
+        let gênero = ''
+        let img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'homem'
+            if (idade >=0 && idade < 13) {
+                // Criança
+                img.setAttribute('src', 'bebehomem.gif')
+            } else if (idade < 30) {
+                // Jovem
+                img.setAttribute('src', 'jovemhomem.gif')
+            } else if (idade < 60) {
+                // Adulto
+                img.setAttribute('src', 'adultohomem.gif')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idosohomem.gif')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'mulher'
+            if (idade >=0 && idade < 13) {
+                // Criança
+                img.setAttribute('src', 'bebemulher.gif')
+            } else if (idade < 30 ){
+                // Jovem
+                img.setAttribute('src', 'jovemmulher.gif')
+            } else if (idade < 60) {
+                // Adulto
+                img.setAttribute('src', 'adultamulher.gif')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idosamulher.gif')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
